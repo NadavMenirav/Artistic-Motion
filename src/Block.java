a325893bef71986561ce5c92df737d7a8614de15
NadavMenirav <nmenirav@gmail.com>
2024-06-10 15:44:27 +0300

    (feat) Block - now implements isHittingPointHorizontally

--- src/Block.java
+++ src/Block.java
@@ -11,7 +11,53 @@ public class Block implements Collidable {
      * Getter of the shape field.
      * @return A copy of the shape field
      */
-    public Rectangle getCollisionShape() {
+    public Rectangle getCollisionRectangle() {
         return new Rectangle(shape);
+    }
+
+    /**
+     * The function checks whether the hitting Point is horizontal.
+     * @param collisionPoint The collision point we check
+     * @return true if the collision point is horizontal, false otherwise
+     */
+    public boolean isHittingPointHorizontally(Point collisionPoint) {
+        /*
+         * We know that if the point is hitting horizontally, its x value should be equals to one of the vertexes
+         * and the y value should be between the top and bottom edges
+         */
+        boolean isYInRange =
+                Threshold.isDoubleGreaterEqual(
+                        this.shape.getUpperLeft().getY() + this.shape.getHeight(),
+                        collisionPoint.getY()
+                )
+                && Threshold.isDoubleGreaterEqual(
+                        collisionPoint.getY(),
+                        this.shape.getUpperLeft().getY()
+                );
+
+        boolean hittingLeftEdge =
+                Threshold.isDoubleGreaterEqual(collisionPoint.getX(), this.shape.getUpperLeft().getX())
+                && isYInRange;
+
+
+        boolean hittingRightEdge =
+                Threshold.isDoubleGreaterEqual(
+                        this.shape.getUpperLeft().getX() + this.shape.getWidth(),
+                        collisionPoint.getX()
+                )
+                && isYInRange;
+
+        return hittingLeftEdge || hittingRightEdge;
+    }
+
+    /**
+     * The function will be used whenever an object hits this block.
+     * It will do the necessary changes and will return the new velocity of the hitting object
+     * @param collisionPoint The collision point between the hitting object and the collidable one
+     * @param currentVelocity The current velocity of the hitting object
+     * @return The new Velocity that needs to be applied on the hitting object
+     */
+    public Velocity hit(Point collisionPoint, Velocity currentVelocity) {
+
     }
 }
--- src/Collidable.java
+++ src/Collidable.java
@@ -18,4 +18,18 @@ public interface Collidable {
      * @return The new Velocity that needs to be applied on the hitting object
      */
     Velocity hit(Point collisionPoint, Velocity currentVelocity);
+
+    /**
+     * The function checks whether the hitting Point is horizontal.
+     * @param collisionPoint The collision point we check
+     * @return true if the collision point is horizontal, false otherwise
+     */
+    boolean isHittingPointHorizontally(Point collisionPoint);
+
+    /**
+     * The function checks whether the hitting Point is Vertical.
+     * @param collisionPoint The collision point we check
+     * @return true if the collision point is vertical, false otherwise
+     */
+    boolean isHittingPointVertically(Point collisionPoint);
 }
