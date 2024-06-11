8735559671930adce15b40d6a810ac032204e82e
NadavMenirav <nmenirav@gmail.com>
2024-06-11 14:18:06 +0300

    (patch) Sprite interface

---
+++ src/Sprite.java
@@ -0,0 +1,20 @@
+//Nadav Menirav 330845678
+
+import biuoop.DrawSurface;
+
+/**
+ * Sprite interface. Represents the objects that can be drawn on the screen
+ */
+public interface Sprite {
+    /**
+     * This function draws the sprite on the screen.
+     * @param d the DrawSurface we draw on
+     */
+    void drawOn(DrawSurface d);
+
+    /**
+     * This function notifies the spite that a certain period of time has passed,
+     * so he will be able to do its necessary changes.
+     */
+    void timePassed();
+}
