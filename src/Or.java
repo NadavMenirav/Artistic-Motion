1fcd4a1944aeedde3beea7164cff4cda22dff28a
NadavMenirav <nmenirav@gmail.com>
2024-06-29 17:04:05 +0300

    (feat) Or class - now implements nandify()

--- src/Or.java
+++ src/Or.java
@@ -25,4 +25,12 @@ public class Or extends BinaryExpression {
              this.getSecondExpression().assign(var, expression)
         );
     }
+
+    @Override
+    public Expression nandify() {
+        return new Nand(
+            new Nand(this.getFirstExpression(), this.getFirstExpression()),
+            new Nand(this.getSecondExpression(), this.getSecondExpression())
+        );
+    }
 }
