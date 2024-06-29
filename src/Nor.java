91263e3d20419cd5b00019929c0663792ae1c56a
NadavMenirav <nmenirav@gmail.com>
2024-06-29 15:14:35 +0300

    (feat) Nor class

---
+++ src/Nor.java
@@ -0,0 +1,28 @@
+//Nadav Menirav 330845678
+
+/**
+ * Nor class.
+ */
+public class Nor extends BinaryExpression {
+    /**
+     * Constructor.
+     * @param firstExpression First Expression
+     * @param secondExpression Second Expression
+     */
+    public Nor(Expression firstExpression, Expression secondExpression) {
+        super(firstExpression, secondExpression, "V");
+    }
+
+    @Override
+    public Boolean operate(Boolean first, Boolean second) {
+        return !(first || second);
+    }
+
+    @Override
+    public Expression assign(String var, Expression expression) {
+        return new Nor(
+            this.getFirstExpression().assign(var, expression),
+             this.getSecondExpression().assign(var, expression)
+        );
+    }
+}
