4f2d5c8505d8ad933eee3066aa5bcd90e3bd8c10
NadavMenirav <nmenirav@gmail.com>
2024-06-29 15:16:58 +0300

    (feat) Xnor class

---
+++ src/Xnor.java
@@ -0,0 +1,28 @@
+//Nadav Menirav 330845678
+
+/**
+ * Xnor class.
+ */
+public class Xnor extends BinaryExpression {
+    /**
+     * Constructor.
+     * @param firstExpression First Expression
+     * @param secondExpression Second Expression
+     */
+    public Xnor(Expression firstExpression, Expression secondExpression) {
+        super(firstExpression, secondExpression, "#");
+    }
+
+    @Override
+    public Boolean operate(Boolean first, Boolean second) {
+        return !(first ^ second);
+    }
+
+    @Override
+    public Expression assign(String var, Expression expression) {
+        return new Xnor(
+            this.getFirstExpression().assign(var, expression),
+             this.getSecondExpression().assign(var, expression)
+        );
+    }
+}
