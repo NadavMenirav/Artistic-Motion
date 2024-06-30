ab9ea269d3bfc79c02ad3dba9c404dff5d72329d
NadavMenirav <nmenirav@gmail.com>
2024-06-30 17:43:29 +0300

    (feat) Xor class - simplify()

--- src/Xor.java
+++ src/Xor.java
@@ -22,30 +22,69 @@ public class Xor extends BinaryExpression {
     public Expression assign(String var, Expression expression) {
         return new Xor(
             this.getFirstExpression().assign(var, expression),
-             this.getSecondExpression().assign(var, expression)
+            this.getSecondExpression().assign(var, expression)
         );
     }
 
     @Override
     public Expression nandify() {
-        return new Or(
-            new And(
-                new Not(
-                    this.getFirstExpression().nandify()
-                ).nandify(),
-                this.getSecondExpression().nandify()
-            ).nandify(),
-            new And(
-                this.getFirstExpression().nandify(),
-                new Not(
-                    this.getSecondExpression().nandify()
-                ).nandify()
-            ).nandify()
-        ).nandify();
+        // A XOR B = [ A NAND ( A NAND B ) ] NAND [ B NAND ( A NAND B ) ]
+        Nand nandOfFirstAndSecond = new Nand(this.getFirstExpression(), this.getSecondExpression());
+        return new Nand(
+            new Nand(this.getFirstExpression(), nandOfFirstAndSecond),
+            new Nand(this.getSecondExpression(), nandOfFirstAndSecond)
+        );
     }
 
     @Override
     public Expression norify() {
-        return this.nandify().norify();
+        // A XOR B =  [ ( A NOR A ) NOR ( B NOR B ) ] NOR ( A NOR B )
+        Expression firstNorified = this.getFirstExpression().norify();
+        Expression secondNorified = this.getSecondExpression().norify();
+        Expression norOfFirsts = new Nor(firstNorified, firstNorified);
+        Expression norOfSeconds = new Nor(secondNorified, secondNorified);
+        Expression norFirstSecond = new Nor(firstNorified, secondNorified);
+
+        return new Nor(new Nor(norOfFirsts, norOfSeconds), norFirstSecond);
     }
+
+    @Override
+    public Expression simplify() {
+      Expression simpleFirst = this.getFirstExpression().simplify();
+      Expression simpleSecond = this.getSecondExpression().simplify();
+
+      // Deals with the case in which one of the expressions is true and the other is false
+      if ((simpleFirst.toString().equals("T") && simpleSecond.toString().equals("F"))
+      || (simpleFirst.toString().equals("F") && simpleSecond.toString().equals("T"))) {
+        return new Val(true);
+      }
+
+      // Both of the expressions are the same
+      if (simpleFirst.toString().equals(simpleSecond.toString())) {
+        return new Val(false);
+      }
+
+      // First is true (We know second is not a Boolean value)
+      if (simpleFirst.toString().equals("T")) {
+        return new Not(simpleSecond);
+      }
+
+      // Second is true (We know first is not a Boolean value)
+      if (simpleSecond.toString().equals("T")) {
+        return new Not(simpleFirst);
+      }
+
+      // First is false (We know second is not a Boolean value)
+      if (simpleFirst.toString().equals("F")) {
+        return simpleSecond;
+      }
+
+      // Second is false (We know first is not a Boolean value)
+      if (simpleSecond.toString().equals("F")) {
+        return simpleFirst;
+      }
+
+      // No further simplification was found
+      return new Xor(simpleFirst, simpleSecond);
+  }
 }
