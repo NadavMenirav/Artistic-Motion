c92e1c2d87188ce6da5d4bafd2471996eee3c97f
NadavMenirav <nmenirav@gmail.com>
2024-06-30 10:32:53 +0300

    (Refactor) UnaryExpression

--- src/UnaryExpression.java
+++ src/UnaryExpression.java
@@ -1,5 +1,8 @@
 //Nadav Menirav 330845678
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * UnaryExpression class.
  */
@@ -27,4 +30,35 @@ public abstract class UnaryExpression extends BaseExpression {
     public Boolean equals(Expression other) {
         return this.toString().equals(other.toString());
     }
+
+    /**
+     * Getter of the expression field.
+     * @return The expression
+     */
+    public Expression getExpression() {
+        return this.expression;
+    }
+
+    @Override
+    public Boolean evaluate(Map<String, Boolean> assignment) throws Exception {
+        return operate(this.expression.evaluate(assignment));
+    }
+
+    @Override
+    public Boolean evaluate() throws Exception {
+        return operate(this.expression.evaluate());
+    }
+
+    @Override
+    public List<String> getVariables() {
+        return this.expression.getVariables();
+    }
+
+    @Override
+    public String toString() {
+        return this.getExpressionSymbol() + "(" + this.expression.toString() + ")";
+    }
+    @Override
+    public abstract Expression assign(String var, Expression expression);
+
 }
