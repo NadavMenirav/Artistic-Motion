28ea49fac7289f3777c47582d82c9cefa14b5346
NadavMenirav <nmenirav@gmail.com>
2024-06-30 18:53:05 +0300

    (feat) Or class - reduced visibility to protected

--- src/Or.java
+++ src/Or.java
@@ -14,7 +14,7 @@ public class Or extends BinaryExpression {
     }
 
     @Override
-    public Boolean operate(Boolean first, Boolean second) {
+    protected Boolean operate(Boolean first, Boolean second) {
         return first || second;
     }
 
