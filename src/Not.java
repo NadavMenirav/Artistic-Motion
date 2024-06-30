2f6b686237bf28cb13903a2bb1c1d745a003579e
NadavMenirav <nmenirav@gmail.com>
2024-06-30 10:36:08 +0300

    (Refactor) Not

--- src/Not.java
+++ src/Not.java
@@ -1,8 +1,5 @@
 //Nadav Menirav 330845678
 
-import java.util.Map;
-import java.util.List;
-
 /**
  * Not class.
  */
@@ -21,33 +18,13 @@ public final class Not extends UnaryExpression {
     }
 
     @Override
-    public Boolean evaluate(Map<String, Boolean> assignment) throws Exception {
-        return !(this.expression.evaluate(assignment));
-    }
-
-    @Override
-    public Boolean evaluate() throws Exception {
-        return !(this.expression.evaluate());
-    }
-
-    @Override
-    public List<String> getVariables() {
-        return this.expression.getVariables();
-    }
-
-    @Override
-    public String toString() {
-        return "~" + "(" + this.expression.toString() + ")";
-    }
-
-    @Override
     public Expression assign(String var, Expression expression) {
-        return new Not(this.expression.assign(var, expression));
+        return new Not(this.getExpression().assign(var, expression));
     }
 
     @Override
     public Expression nandify() {
-        return new Nand(this.expression.nandify(), this.expression.nandify());
+        return new Nand(this.getExpression().nandify(), this.getExpression().nandify());
     }
 
     @Override
