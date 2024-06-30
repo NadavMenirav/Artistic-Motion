81764c88a2fdde1bba1922d6a2f8ed4848135ad4
NadavMenirav <nmenirav@gmail.com>
2024-06-30 09:41:09 +0300

    (feat) BaseExpression class

--- src/And.java
+++ src/And.java
@@ -35,4 +35,28 @@ public class And extends BinaryExpression {
     public Expression norify() {
         return this.nandify().norify();
     }
+
+    @Override
+    public Expression simplify() {
+        try {
+            //1&x case
+            if (this.getFirstExpression().evaluate()) {
+                return this.getSecondExpression().simplify();
+            }
+            //0&x case
+            return new Val(false);
+        } catch (Exception error) {
+            System.err.println(error);
+        }
+        try {
+            //x&1 case
+            if (this.getSecondExpression().evaluate()) {
+                return this.getFirstExpression().simplify();
+            }
+            //x&0 case
+            return new Val(false);
+        } catch (Exception error) {
+            System.err.println(error);
+        }
+    }
 }
