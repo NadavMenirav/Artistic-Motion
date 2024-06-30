021abe5e5c8e5ffe11f5d75dbea8fb93574e60bb
NadavMenirav <nmenirav@gmail.com>
2024-06-30 19:03:23 +0300

    (feat) Xor class - reduced visibility to protected

--- src/Xor.java
+++ src/Xor.java
@@ -14,7 +14,7 @@ public class Xor extends BinaryExpression {
     }
 
     @Override
-    public Boolean operate(Boolean first, Boolean second) {
+    protected Boolean operate(Boolean first, Boolean second) {
         return first ^ second;
     }
 
