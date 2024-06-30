06be8db847883cb8f31817eace710603a0015ab4
NadavMenirav <nmenirav@gmail.com>
2024-06-30 16:55:10 +0300

    (feat) Nor class - simplify()

--- src/Nor.java
+++ src/Nor.java
@@ -22,12 +22,13 @@ public class Nor extends BinaryExpression {
     public Expression assign(String var, Expression expression) {
         return new Nor(
             this.getFirstExpression().assign(var, expression),
-             this.getSecondExpression().assign(var, expression)
+            this.getSecondExpression().assign(var, expression)
         );
     }
 
     @Override
     public Expression nandify() {
+        // A NOR B = [ ( A NAND A ) NAND ( B NAND B ) ] NAND [ ( A NAND A ) NAND ( B NAND B ) ]
         return new Not(
             new Or(
                 this.getFirstExpression().nandify(),
@@ -38,6 +39,35 @@ public class Nor extends BinaryExpression {
 
     @Override
     public Expression norify() {
-        return this.nandify().norify();
+        // We just need to norify the inner expressions
+        return new Nor(this.getFirstExpression().norify(), this.getSecondExpression().norify());
+    }
+
+    @Override
+    public Expression simplify() {
+
+        Expression simpleFirst = this.getFirstExpression().simplify();
+        Expression simpleSecond = this.getSecondExpression().simplify();
+
+        // Handles the case where one of the expressions is true
+        if (simpleFirst.toString().equals("T") || simpleSecond.toString().equals("T")) {
+            return new Val(false);
+        }
+
+        // First expression is false
+        if (simpleFirst.toString().equals("F")) {
+            return new Not(simpleSecond);
+        }
+
+        // Second is false
+        if (simpleSecond.toString().equals("F")) {
+            return new Not(simpleFirst);
+        }
+
+        // Both expressions are the same
+        if (simpleFirst.toString().equals(simpleSecond.toString())) {
+            return new Not(simpleFirst);
+        }
+        return new Nor(simpleFirst, simpleSecond);
     }
 }
