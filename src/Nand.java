0abd5a7594fb483fbed959f51a2a5df772be29fb
NadavMenirav <nmenirav@gmail.com>
2024-06-30 16:43:30 +0300

    (feat) Nand - simplify()

--- src/Nand.java
+++ src/Nand.java
@@ -42,4 +42,38 @@ public class Nand extends BinaryExpression {
         Expression norSecond = new Nor(secondNorified, secondNorified);
         return new Nor(new Nor(norFirst, norSecond), new Nor(norFirst, norSecond));
     }
+
+    @Override
+    public Expression simplify() {
+        Expression simpleFirst = this.getFirstExpression().simplify();
+        Expression simpleSecond = this.getSecondExpression().simplify();
+
+        // Handles a case where both expressions are true
+        if (simpleFirst.toString().equals("T") && simpleSecond.toString().equals("T")) {
+            return new Val(false);
+        }
+
+        // One of the expressions are false
+        if (simpleFirst.toString().equals("F") || simpleSecond.toString().equals("F")) {
+            return new Val(true);
+        }
+
+        // First is true
+        if (simpleFirst.toString().equals("T")) {
+            return new Not(simpleSecond);
+        }
+
+        // Second is true
+        if (simpleSecond.toString().equals("T")) {
+            return new Not(simpleFirst);
+        }
+
+        // Both expressions are the same
+        if (simpleFirst.toString().equals(simpleSecond.toString())) {
+            return new Not(simpleFirst);
+        }
+
+        // No further simplification was found
+        return new Nand(simpleFirst, simpleSecond);
+    }
 }
