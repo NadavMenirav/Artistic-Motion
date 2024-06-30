34b9ded8d4fefc92b51280bacc601fc2e6283846
NadavMenirav <nmenirav@gmail.com>
2024-06-30 17:32:47 +0300

    (feat) Xnor class - simplify()

--- src/Xnor.java
+++ src/Xnor.java
@@ -22,22 +22,48 @@ public class Xnor extends BinaryExpression {
     public Expression assign(String var, Expression expression) {
         return new Xnor(
             this.getFirstExpression().assign(var, expression),
-             this.getSecondExpression().assign(var, expression)
+            this.getSecondExpression().assign(var, expression)
         );
     }
 
     @Override
     public Expression nandify() {
-        return new Not(
-            new Xor(
+        // A XNOR B = [ ( A NAND A ) NAND ( B NAND B ) ] NAND ( A NAND B )
+        return new Nand(
+            new Nand(
+                new Not(this.getFirstExpression().nandify()).nandify(),
+                new Not(this.getSecondExpression().nandify()).nandify()
+            ),
+            new Nand(
                 this.getFirstExpression().nandify(),
                 this.getSecondExpression().nandify()
-            ).nandify()
-        ).nandify();
+            )
+        );
     }
 
     @Override
     public Expression norify() {
-        return this.nandify().norify();
+        // A XNOR B = [ A NOR ( A NOR B ) ] NOR [ B NOR ( A NOR B ) ]
+        Expression first = this.getFirstExpression().norify();
+        Expression second = this.getSecondExpression().norify();
+        return new Nor(new Nor(first, new Nor(first, second)), new Nor(second, new Nor(first, second)));
+    }
+
+    @Override
+    public Expression simplify() {
+        Expression simpleFirst = this.getFirstExpression().simplify();
+        Expression simpleSecond = this.getSecondExpression().simplify();
+
+        // Deals with the Case of which one of the expressions is true and the other is false
+        if ((simpleFirst.toString().equals("T") && simpleSecond.toString().equals("F"))
+        || (simpleFirst.toString().equals("F") && simpleSecond.toString().equals("T"))) {
+            return new Val(false);
+        }
+
+        // Both of the expressions are the same
+        if (simpleFirst.toString().equals(simpleSecond.toString())) {
+            return new Val(true);
+        }
+        return new Xnor(simpleFirst, simpleSecond);
     }
 }
