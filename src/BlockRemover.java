312dfd750033ac55943ded9025f8058f85ebc9bf
NadavMenirav <nmenirav@gmail.com>
2024-07-08 18:28:08 +0300

    (feat) BlockRemover class

---
+++ src/BlockRemover.java
@@ -0,0 +1,32 @@
+import org.w3c.dom.css.Counter;
+
+/**
+ * A BlockRemover is in charge of removing blocks from the game, as well as keeping count.
+ * of the number of blocks that remain.
+ */
+public class BlockRemover {
+    private Game game;
+    private Counter remainingBlocks;
+
+    /**
+     * Constructor of the BlockRemover class.
+     * @param game The game of this blockRemover
+     * @param remainingBlocks The remaining blocks in the blockRemover
+     */
+    public BlockRemover(Game game, Counter remainingBlocks) {
+        this.game = game;
+        this.remainingBlocks = remainingBlocks;
+    }
+
+    /**
+     * Blocks that are hit should be removed.
+     * Remember to remove this listener from the block that is being removed from the game.
+     * @param beingHit The block being hit
+     * @param hitter The Ball hitting
+     */
+    public void hitEvent(Block beingHit, Ball hitter) {
+        beingHit.removeHitListener(this);
+        beingHit.removeFromGame(game);
+        remainingBlocks.decrease(1);
+    }
+}
