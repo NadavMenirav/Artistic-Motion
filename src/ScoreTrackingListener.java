abb71f218c54df542f6c25d4e85bc1b0167c0475
NadavMenirav <nmenirav@gmail.com>
2024-07-09 17:28:01 +0300

    (feat) ScoreTrackingListener class

---
+++ src/ScoreTrackingListener.java
@@ -0,0 +1,15 @@
+
+/**
+ * ScoreTrackingListener class.
+ */
+public class ScoreTrackingListener {
+    private final Counter currentScore;
+
+    /**
+     * Constructor of the ScoreTrackingListener class.
+     * @param scoreCounter TThe scoreCounter
+     */
+    public ScoreTrackingListener(Counter scoreCounter) {
+        this.currentScore = scoreCounter;
+    }
+}
